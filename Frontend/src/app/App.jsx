61e88d2e7ff61695b8fdcaa40b8a93685f3751a8
Akshit Ramola <ramolaakshit1@gmail.com>
2026-04-13 23:16:47 +0530

    Update React routing setup

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes'
 
 function App() {
 
 
   return (
     <>
+      <RouterProvider router={routes} />
     </>
   )
 }
